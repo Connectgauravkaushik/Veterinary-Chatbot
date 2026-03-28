991635a7fb480c388f390792078109f726cec133
Gaurav Kaushik <101702222+Connectgauravkaushik@users.noreply.github.com>
2026-03-28 13:34:43 +0530

    Add binary search code

--- test_py.py
+++ test_py.py
@@ -1,26 +1,27 @@
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
 
-    def binary_search(arr, target):
-        low = 0
-        high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        mid_val = arr[mid]
 
-        while low <= high:
-            mid = (low + high) // 2
-            if arr[mid] == target:
-                return mid
-            elif arr[mid] < target:
-                low = mid + 1
-            else:
-                high = mid - 1
+        if mid_val == target:
+            return mid
+        elif mid_val < target:
+            low = mid + 1
+        else:
+            high = mid - 1
 
-        return -1  # Target not found
+    return -1
 
-    # Example usage:
-    my_list = [1, 3, 5, 7, 9, 11, 13, 15]
-    search_value = 7
-    index = binary_search(my_list, search_value)
+# Example usage:
+my_list = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+search_value = 11
 
-    if index != -1:
-        print(f"Target {search_value} found at index {index}")
-    else:
-        print(f"Target {search_value} not found in the list")
-    
+index = binary_search(my_list, search_value)
+
+if index != -1:
+    print(f"Element {search_value} found at index {index}")
+else:
+    print(f"Element {search_value} not found in the list")
